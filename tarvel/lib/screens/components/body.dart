b41d083ff6ce7d00b0cd0b2ecf04a7318af7c73f
gun3486 <gun3486@naver.com>
2021-03-15 21:11:58 +0900

    t
    t

--- tarvel/lib/screens/components/body.dart
+++ tarvel/lib/screens/components/body.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:travel/constants.dart';
 import 'package:travel/screens/components/search_field.dart';
-import 'package:travel/size_config.dart';
 import 'home_header.dart';
 
 class Body extends StatelessWidget {
