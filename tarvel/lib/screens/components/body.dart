7292713f7aae4464ff498825a225c1feeb19c0de
manuld <49752705+manuld@users.noreply.github.com>
2021-03-15 21:00:15 +0900

    서치버튼홈헤더
    서치버튼과홈헤더를만듬

--- tarvel/lib/screens/components/body.dart
+++ tarvel/lib/screens/components/body.dart
@@ -2,44 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:travel/constants.dart';
 import 'package:travel/screens/components/search_field.dart';
 import 'package:travel/size_config.dart';
+import 'home_header.dart';
 
 class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Stack(
-          overflow: Overflow.visible,
-          alignment: Alignment.center,
-          children: [
-            Image.asset("assets/images/home_bg.png",
-            height: getProportionateScreenWidth(315),
-            fit: BoxFit.cover,
-            ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SizedBox(height: getProportionateScreenHeight(80),),
-                Text(
-                  "Travelers",
-                  style: TextStyle(fontSize: getProportionateScreenWidth(73),
-                  fontWeight: FontWeight.bold,
-                  color: Colors.tealAccent
-                  ),
-                ),
-                Text(
-                  'Travel Community App250',
-                  style: TextStyle(color: Colors.white),
-                )
-              ],
-            ),
-            Positioned(
-              bottom: getProportionateScreenWidth(-25),
-              child: SearchField(),
-            )
-          ],
-        ),
+        HomeHeader(),
       ],
     );
   }
 }
+
---
+++ tarvel/lib/screens/components/home_header.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:travel/screens/components/search_field.dart';
+import '../../size_config.dart';
+
+class HomeHeader extends StatelessWidget {
+  const HomeHeader({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      overflow: Overflow.visible,
+      alignment: Alignment.center,
+      children: [
+        Image.asset("assets/images/home_bg.png",
+        height: getProportionateScreenWidth(315),
+        fit: BoxFit.cover,
+        ),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(height: getProportionateScreenHeight(80),),
+            Text(
+              "Travelers",
+              style: TextStyle(fontSize: getProportionateScreenWidth(73),
+              fontWeight: FontWeight.bold,
+              color: Colors.tealAccent
+              ),
+            ),
+            Text(
+              'Travel Community App250',
+              style: TextStyle(color: Colors.white),
+            )
+          ],
+        ),
+        Positioned(
+          bottom: getProportionateScreenWidth(-25),
+          child: SearchField(),
+        )
+      ],
+    );
+  }
+}
