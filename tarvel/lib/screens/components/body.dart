6a777107f7d8b6c7f180e6dd453e3312deb46bd0
gun3486 <gun3486@naver.com>
2021-03-16 20:56:54 +0900

    타이틀섹션만듬
    타이틀섹션

--- tarvel/lib/screens/components/body.dart
+++ tarvel/lib/screens/components/body.dart
@@ -1,15 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:travel/components/section_title.dart';
+import 'package:travel/constants.dart';
+import 'package:travel/models/TravelSpot.dart';
 import 'package:travel/screens/components/search_field.dart';
+import 'package:travel/size_config.dart';
 import 'home_header.dart';
 
 class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        HomeHeader(),
-      ],
+    return SingleChildScrollView(
+      clipBehavior: Clip.none,
+      child: Column(
+        children: [
+          HomeHeader(),
+          VerticalSpacing(),
+          SectionTitle(
+            title: "Right Now At Spark",
+            press: () {},
+          ),
+          SizedBox(
+            width: getProportionateScreenWidth(137),
+            child: Column(
+              children: [
+                AspectRatio(
+                  aspectRatio: 1.29,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(20),
+                        topRight: Radius.circular(20)
+                      ),
+                      image: DecorationImage(
+                        image: AssetImage(travelSpots[0].image)
+                      )
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
-
